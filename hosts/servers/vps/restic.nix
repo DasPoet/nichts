71536ca5ca3b5e78791994d35bea2db6130f32c0
daspoet <git@daspoet.dev>
2025-04-13 04:04:44 +0200

    feat: backup mailcow

--- hosts/servers/vps/restic.nix
+++ hosts/servers/vps/restic.nix
@@ -1,11 +1,13 @@
-{ config, ... }:
+{ config, meta, ... }:
 
 {
   services.restic.backups = {
     local = {
       initialize = true;
       repository = "/var/backup/restic/local";
-      paths = []; # TODO
+      paths = [
+        "${meta.user.name}/github/postkuh"
+      ];
       passwordFile = config.sops.secrets."restic/secret".path;
     };
   };
