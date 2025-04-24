255d00655f05c44aa9b5169cbcb51a7c8a5fdf13
Erkens, Cedric <cedric.erkens@soptim.de>
2025-04-24 15:25:52 +0200

    feat: configure more SOPTIM DNS servers

--- hosts/soptim-wsl/dns.nix
+++ hosts/soptim-wsl/dns.nix
@@ -4,6 +4,8 @@
     nameservers = [
       "10.18.100.101" # SOPTIM 1
       "10.18.100.100" # SOPTIM 2
+      "10.20.100.101" # SOPTIM 3
+      "10.20.100.100" # SOPTIM 4
       "8.8.8.8"       # Google DNS
       "8.8.4.4"       # Google DNS backup
       "1.1.1.1"       # Cloudflare
@@ -11,6 +13,5 @@
     search = [
       "soptim.net"
     ];
-    # If your company uses specific internal DNS servers, add them here instead
   };
 }
